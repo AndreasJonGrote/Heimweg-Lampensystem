75b74c26ff5bb1ae1bb4a8e1ec6937e949c9346a
Andreas Jon Grote <12413794+AndreasJonGrote@users.noreply.github.com>
2025-05-16 08:51:49 +0200

    Update Master-Display.ino

--- Display/Master-Display/Master-Display.ino
+++ Display/Master-Display/Master-Display.ino
@@ -1,23 +1,43 @@
-#include <Adafruit_GFX.h>
-#include <Adafruit_ST7735.h>
-#include <SPI.h>
+/*
+ * Master-Display.ino
+ * 
+ * Dieses Programm steuert ein ST7735 TFT-Display an und implementiert
+ * grundlegende Textdarstellungsfunktionen mit Unterstützung für:
+ * - Deutsche Umlaute (UTF-8 zu ASCII Konvertierung)
+ * - Automatischer Zeilenumbruch
+ * - Konfigurierbares Padding
+ * - Verschiedene Textgrößen und Farben
+ */
 
-// TFT-Pins
-#define TFT_CS     5
-#define TFT_RST    4
-#define TFT_DC     2
+#include <Adafruit_GFX.h>    // Grafik-Bibliothek für verschiedene Displays
+#include <Adafruit_ST7735.h> // Spezifische Bibliothek für ST7735 TFT-Displays
+#include <SPI.h>             // SPI-Kommunikation für Display-Ansteuerung
 
+// Definition der Display-Pins
+#define TFT_CS     5        // Chip Select Pin
+#define TFT_RST    4        // Reset Pin
+#define TFT_DC     2        // Data/Command Pin
+
+// Initialisierung des Display-Objekts
 Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
 
-// Padding und Textlayout
-const int padding = 10;
-int currentY = padding;
+// Globale Layout-Einstellungen
+const int padding = 10;     // Randabstand in Pixeln
+int currentY = padding;     // Aktuelle Y-Position für Textausgabe
 
+/**
+ * Setzt die Y-Position für die Textausgabe zurück
+ * Wird verwendet, um von vorne mit dem Schreiben zu beginnen
+ */
 void resetLines() {
   currentY = padding;
 }
 
-// UTF-8-Ersetzung
+/**
+ * Konvertiert deutsche Umlaute und Sonderzeichen in ASCII-Darstellung
+ * @param text Der zu konvertierende Text
+ * @return String mit ersetzten Umlauten
+ */
 String utf8ToAscii(String text) {
   text.replace("ä", "ae");
   text.replace("ö", "oe");
@@ -31,15 +51,22 @@ String utf8ToAscii(String text) {
   return text;
 }
 
-// Text zeichnen mit Padding, Umbruch und Farben
+/**
+ * Zeichnet eine Textzeile mit automatischem Umbruch
+ * @param text Der darzustellende Text
+ * @param size Textgröße (Standard: 1)
+ * @param color Textfarbe (Standard: Weiß)
+ */
 void drawWrappedLine(String text, int size = 1, uint16_t color = ST77XX_WHITE) {
   String textCopy = utf8ToAscii(text);
 
-  int charWidth = 6 * size;
-  int maxLineWidth = tft.width() - 2 * padding;
-  int charsPerLine = maxLineWidth / charWidth;
-  int lineHeight = 8 * size + 2;
+  // Berechnung der Textdimensionen
+  int charWidth = 6 * size;                    // Breite eines Zeichens
+  int maxLineWidth = tft.width() - 2 * padding; // Maximale Zeilenbreite
+  int charsPerLine = maxLineWidth / charWidth;  // Zeichen pro Zeile
+  int lineHeight = 8 * size + 2;               // Zeilenhöhe mit Abstand
 
+  // Text in Zeilen aufteilen und ausgeben
   for (int i = 0; i < textCopy.length(); i += charsPerLine) {
     String line = textCopy.substring(i, min(i + charsPerLine, (int)textCopy.length()));
     tft.setTextSize(size);
@@ -50,18 +77,29 @@ void drawWrappedLine(String text, int size = 1, uint16_t color = ST77XX_WHITE) {
   }
 }
 
+/**
+ * Setup-Routine: Wird einmalig beim Start ausgeführt
+ * Initialisiert das Display und zeigt Beispieltext an
+ */
 void setup() {
-  Serial.begin(115200);
-  tft.initR(INITR_BLACKTAB);
-  tft.setRotation(0);
-  tft.fillScreen(ST77XX_BLACK);
-  resetLines();
+  Serial.begin(115200);               // Seriellen Monitor initialisieren
+  
+  // Display-Initialisierung
+  tft.initR(INITR_BLACKTAB);         // Spezifische Display-Variante
+  tft.setRotation(0);                // Portrait-Modus
+  tft.fillScreen(ST77XX_BLACK);      // Bildschirm schwarz färben
+  resetLines();                      // Textposition zurücksetzen
 
+  // Beispieltext zum Testen der Funktionen
   drawWrappedLine("Padding links & rechts – passt!", 1);
   drawWrappedLine("Große Wörter wie Übersetzung oder äußert erscheinen korrekt.", 1);
   drawWrappedLine("Noch mehr Text in Rot, der umbricht.", 1);
 }
 
+/**
+ * Hauptschleife: Wird kontinuierlich ausgeführt
+ * Aktuell keine Funktionalität implementiert
+ */
 void loop() {
-  // Nix hier
+  // Keine Aktionen in der Hauptschleife
 }
